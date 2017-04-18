499bbe66f952111bfacdff7ec08f8e5a7356eb1a
Ha Manh Do <hadm.haui@gmail.com>
2017-04-18 08:08:12 +0700

    Pro Angular - JavaScript & TypeScript

---
+++ apress/pro-angular/c5-javascript-typescript/primer.js
@@ -0,0 +1,233 @@
+console.log("Hello");
+console.log("Apples");
+console.log("This is a statement");
+console.log("This is also a statement");
+var myFunc = function () {
+    console.log("This is a statement");
+};
+function myFunc2() {
+    console.log("This is another statement");
+}
+myFunc();
+myFunc2();
+var myFunc3 = function (name, weather) {
+    if (weather === void 0) { weather = "raining"; }
+    var extraArgs = [];
+    for (var _i = 2; _i < arguments.length; _i++) {
+        extraArgs[_i - 2] = arguments[_i];
+    }
+    console.log("Hello " + name + ".");
+    console.log("It is " + weather + " today.");
+    for (var i = 0; i < extraArgs.length; i++) {
+        console.log("Extra arg: " + extraArgs[i]);
+    }
+};
+myFunc3("Adam", "sunny");
+myFunc3("Anna");
+myFunc3("Eric", "cloudy", "one", "two", "three");
+var myFunc4 = function (name) {
+    return ("Hello " + name + ".");
+};
+console.log(myFunc4("Micheal"));
+var myFunc5 = function (nameFunction) {
+    return ("Hello " + nameFunction() + ".");
+};
+console.log(myFunc5(function () {
+    return "Charlie";
+}));
+var myFunc6 = function (nameFunction) {
+    return ("Hello " + nameFunction() + ".");
+};
+var printName = function (nameFunction, printFunction) {
+    printFunction(myFunc6(nameFunction));
+};
+printName(function () {
+    return "Antonio";
+}, console.log);
+// Arrow functions
+var myFunc7 = function (nameFunction) { return ("Hello " + nameFunction() + "."); };
+var printName2 = function (nameFunction, printFunction) { return printFunction(myFunc7(nameFunction)); };
+printName2(function () {
+    return "Emma";
+}, console.log);
+// Variables and Types
+var messageFunction = function (name, weather) {
+    // let message = "Hello, Adam";
+    var message = "Hello, Adam";
+    if (weather == "sunny") {
+        // let message = "It is a nice day";
+        var message = "It is a nice day";
+        console.log(message);
+    }
+    else {
+        // let message = "It is " + weather + " today";
+        var message = "It is " + weather + " today";
+        console.log(message);
+    }
+    console.log(message);
+};
+messageFunction("Adam", "raining");
+// Closure
+var myGlobalVar = "apples";
+var myFunc8 = function (name) {
+    var myLocalVar = "sunny";
+    var innerFunction = function () {
+        return ("Hello " + name + ". Today is " + myLocalVar + ".");
+    };
+    return innerFunction();
+};
+console.log(myFunc8("John"));
+var firstBool = true;
+var secondBool = false;
+var firstString = "   This is a string   ";
+firstString = firstString.trim();
+var secondString = "And so is this";
+console.log("String: " + firstString + " has length: " + firstString.length
+    + " and letter at 8 is: '" + firstString.charAt(8) + "', new string: "
+    + firstString.concat(".And a little bit more.") + " and indexOf first 'i' letter is: "
+    + firstString.indexOf('i') + ". new string: " + firstString.replace("little more", "few")
+    + ", sub string from 3 - 12 is: '" + firstString.slice(3, 12) + "' and an array of words: " + firstString.split(" ").join(",")
+    + ". in Uppercase format: " + firstString.toUpperCase() + " and in Lowercase format: " + firstString.toLowerCase());
+// Template string
+var greet = function (weather) {
+    var message = "It is " + weather + " today";
+    console.log(message);
+};
+greet("raining");
+// Numbers
+var daysInWeek = 7;
+var pi = 3.14;
+var hexValue = 0xFFFF;
+console.log(daysInWeek++, daysInWeek--, daysInWeek);
+console.log(true ? true : false);
+// Conditional
+var name2 = "Adam";
+if (name2 == "Adam") {
+    console.log("Name is John");
+}
+else if (name2 == "Jacqui") {
+    console.log("Name is Jacqui");
+}
+else {
+    console.log("Name is neither Adam or Jacqui");
+}
+switch (name2) {
+    case "Adam":
+        console.log("Name is Adam");
+        break;
+    case "Jacqui":
+        console.log("Name is Jacqui");
+        break;
+    default:
+        console.log("Name is neither Adam or Jacqui");
+}
+var firstVal = 5;
+var secondVal = "5";
+// if (firstVal == secondVal) {
+if (firstVal === secondVal) {
+    console.log("They are the same.");
+}
+else {
+    console.log("They are NOT the same");
+}
+var myData1 = 5 + 5;
+var myData2 = 5 + "5";
+console.log("Result 1: " + myData1);
+console.log("Result 2: " + myData2);
+console.log("Result: " + (5).toString() + String(5));
+console.log((5.2).toString(2), (5.2).toString(8), (5.2).toString(16));
+console.log((5.23432432).toFixed(2));
+console.log((5.23432432).toExponential(2));
+console.log((5.23432432).toPrecision(2));
+var firstVal1 = "5.2";
+var secondVal1 = "5.2";
+var result = Number(firstVal1) + Number(secondVal1);
+var resultInt = parseInt(firstVal1) + parseInt(secondVal1);
+var resultFloat = parseFloat(firstVal1) + parseFloat(secondVal1);
+console.log("Result: " + result);
+console.log("Result: " + resultInt);
+console.log("Result: " + resultFloat);
+// Arrays
+var myArray = new Array();
+myArray[0] = 100;
+myArray[1] = "Adam";
+myArray[2] = true;
+var myArray2 = [100, "Adam", true];
+console.log("Index 1: " + myArray[1]);
+for (var i = 0; i < myArray.length; i++) {
+    console.log("Index " + i + ": " + myArray[i]);
+}
+console.log("=====");
+myArray2.forEach(function (value, index) { return console.log("Index " + index + ": " + value); });
+// concat
+var newArray = myArray.concat(myArray2);
+console.log(newArray);
+// join
+var str = newArray.join(",");
+console.log(str);
+// pop
+var pop = newArray.pop();
+console.log(pop, newArray);
+// shift
+var shift = newArray.shift();
+console.log(shift, newArray);
+// push
+newArray.push("end");
+console.log(newArray);
+// unshift
+newArray.unshift("start");
+console.log(newArray);
+// reverse
+console.log(newArray.reverse());
+// slice
+var slice = newArray.slice(2, 4);
+console.log(slice);
+// sort
+console.log(newArray.sort());
+// splice
+var splice = newArray.splice(3, 2);
+console.log(splice, newArray);
+// every: true if all items is true, vice versa false == AND
+console.log(splice.every(function (item) { return typeof item == 'string'; }));
+console.log(newArray.every(function (item) { return typeof item == 'number'; }));
+// some: true if one of the items is true, vice verse false == OR
+console.log(splice.some(function (item) { return typeof item == 'number'; }));
+console.log(newArray.some(function (item) { return typeof item == 'number'; }));
+// filter: return all matched array
+console.log(newArray.filter(function (item) { return item === "Adam"; }));
+// find: return fisrt ele of matched array
+console.log(newArray.find(function (item) { return item === "Adam"; }));
+// findIndex: same as find but return index instead
+console.log(newArray.findIndex(function (item) { return item === "Adam"; }));
+// foreach
+var newStr = '';
+newArray.forEach(function (item, index) {
+    if (index < newArray.length - 1)
+        newStr += item + ",";
+    else
+        newStr += item;
+});
+console.log(newArray, newStr);
+// includes
+console.log(newArray.includes("Adam"));
+// reduce: (previous, current), initialValue
+// [0, 1, [2, [3, [4, [5]]]]]
+// [0, 1, 2, [3, [4, [5]]]]
+// [0, 1, 2, 3, [4, [5]]]
+// [0, 1, 2, 3, 4, [5]]
+// [0, 1, 2, 3, 4, 5]
+var list = [0, [1, [2, [3, [4, [5]]]]]];
+var flatten = function (arr) { return arr.reduce(function (previous, current) { return previous.concat(Array.isArray(current) ? flatten(current) : current // recursive
+); }, []); };
+console.log(flatten(list));
+// Example
+var products = [
+    { name: "Hat", price: 24.5, stock: 10 },
+    { name: "Kayak", price: 289.99, stock: 1 },
+    { name: "Soccer Ball", price: 10, stock: 0 },
+    { name: "Running Shoes", price: 116.50, stock: 20 }
+];
+var totalValue = products
+    .filter(function (item) { return item.stock > 0; })
+    .reduce(function (prev, item) { return prev + (item.price * item.stock); }, 0);
+console.log("Total value: $" + totalValue.toFixed(2));
---
+++ apress/pro-angular/c5-javascript-typescript/primer.ts
@@ -0,0 +1,269 @@
+console.log("Hello")
+console.log("Apples")
+console.log("This is a statement");
+console.log("This is also a statement")
+
+let myFunc = function() {
+    console.log("This is a statement");
+};
+
+function myFunc2() {
+    console.log("This is another statement");
+}
+
+myFunc();
+myFunc2();
+
+let myFunc3 = function(name, weather = "raining", ...extraArgs) {
+    console.log("Hello " + name + ".");
+    console.log("It is " + weather + " today.");
+
+    for(let i = 0; i < extraArgs.length; i++) {
+        console.log("Extra arg: " + extraArgs[i]);
+    }
+}
+
+myFunc3("Adam", "sunny");
+myFunc3("Anna");
+myFunc3("Eric", "cloudy", "one", "two", "three");
+
+let myFunc4 = function(name) {
+    return ("Hello " + name + ".");
+}
+
+console.log(myFunc4("Micheal"));
+
+let myFunc5 = function(nameFunction) {
+    return ("Hello " + nameFunction() + ".");
+}
+
+console.log(myFunc5(function() {
+    return "Charlie";
+}))
+
+let myFunc6 = function(nameFunction) {
+    return ("Hello " + nameFunction() + ".");
+}
+
+let printName = function(nameFunction, printFunction) {
+    printFunction(myFunc6(nameFunction));
+}
+
+printName(function() {
+    return "Antonio";
+}, console.log);
+
+// Arrow functions
+let myFunc7 = nameFunction => ("Hello " + nameFunction() + ".");
+let printName2 = (nameFunction, printFunction) => printFunction(myFunc7(nameFunction));
+printName2(function() {
+    return "Emma";
+}, console.log);
+
+// Variables and Types
+let messageFunction = function(name, weather) {
+    // let message = "Hello, Adam";
+    var message = "Hello, Adam";
+    if (weather == "sunny") {
+        // let message = "It is a nice day";
+        var message = "It is a nice day";
+        console.log(message);
+    } else {
+        // let message = "It is " + weather + " today";
+        var message = "It is " + weather + " today";
+        console.log(message);
+    }
+    console.log(message);
+}
+messageFunction("Adam", "raining");
+
+// Closure
+let myGlobalVar = "apples";
+let myFunc8 = function(name) {
+    let myLocalVar = "sunny";
+    let innerFunction = function() {
+        return("Hello " + name + ". Today is " + myLocalVar + ".");
+    }
+    return innerFunction();
+}
+console.log(myFunc8("John"));
+
+let firstBool = true;
+let secondBool = false;
+let firstString = "   This is a string   ";
+firstString = firstString.trim();
+let secondString = "And so is this";
+console.log("String: " + firstString + " has length: " + firstString.length
+            + " and letter at 8 is: '" + firstString.charAt(8) + "', new string: "
+            + firstString.concat(".And a little bit more.") + " and indexOf first 'i' letter is: "
+            + firstString.indexOf('i') + ". new string: " + firstString.replace("little more", "few")
+            + ", sub string from 3 - 12 is: '" + firstString.slice(3, 12) + "' and an array of words: " + firstString.split(" ").join(",")
+            + ". in Uppercase format: " + firstString.toUpperCase() + " and in Lowercase format: " + firstString.toLowerCase());
+
+// Template string
+let greet = function(weather) {
+    let message = `It is ${weather} today`;
+    console.log(message);
+}
+
+greet("raining");
+
+// Numbers
+let daysInWeek = 7;
+let pi = 3.14;
+let hexValue = 0xFFFF;
+
+console.log(daysInWeek++, daysInWeek--, daysInWeek);
+console.log(true ? true : false);
+
+// Conditional
+let name2 = "Adam";
+if (name2 == "Adam") {
+    console.log("Name is John");
+} else if (name2 == "Jacqui") {
+    console.log("Name is Jacqui");
+} else {
+    console.log("Name is neither Adam or Jacqui");
+}
+
+switch(name2) {
+    case "Adam":
+        console.log("Name is Adam");
+        break;
+    case "Jacqui":
+        console.log("Name is Jacqui");
+        break;
+    default:
+        console.log("Name is neither Adam or Jacqui");
+}
+
+let firstVal = 5;
+let secondVal = "5";
+// if (firstVal == secondVal) {
+if (firstVal === secondVal) {
+    console.log("They are the same.");
+} else {
+    console.log("They are NOT the same");
+}
+
+let myData1 = 5 + 5;
+let myData2 = 5 + "5";
+console.log("Result 1: " + myData1);
+console.log("Result 2: " + myData2);
+console.log("Result: " + (5).toString() + String(5));
+console.log((5.2).toString(2), (5.2).toString(8), (5.2).toString(16));
+console.log((5.23432432).toFixed(2));
+console.log((5.23432432).toExponential(2));
+console.log((5.23432432).toPrecision(2));
+
+let firstVal1= "5.2";
+let secondVal1 = "5.2";
+let result = Number(firstVal1) + Number(secondVal1);
+let resultInt = parseInt(firstVal1) + parseInt(secondVal1);
+let resultFloat = parseFloat(firstVal1) + parseFloat(secondVal1);
+console.log("Result: " + result);
+console.log("Result: " + resultInt);
+console.log("Result: " + resultFloat);
+
+// Arrays
+let myArray = new Array();
+myArray[0] = 100;
+myArray[1] = "Adam";
+myArray[2] = true;
+let myArray2 = [100, "Adam", true];
+console.log("Index 1: " + myArray[1]);
+for(let i = 0; i < myArray.length; i++) {
+    console.log("Index " + i + ": " + myArray[i]);
+}
+console.log("=====");
+myArray2.forEach((value, index) => console.log("Index " + index + ": " + value));
+
+// concat
+let newArray = myArray.concat(myArray2);
+console.log(newArray);
+
+// join
+let str = newArray.join(",");
+console.log(str);
+
+// pop
+let pop = newArray.pop();
+console.log(pop, newArray);
+
+// shift
+let shift = newArray.shift();
+console.log(shift, newArray);
+
+// push
+newArray.push("end");
+console.log(newArray);
+
+// unshift
+newArray.unshift("start");
+console.log(newArray);
+
+// reverse
+console.log(newArray.reverse())
+
+// slice
+let slice = newArray.slice(2, 4);
+console.log(slice);
+
+// sort
+console.log(newArray.sort())
+
+// splice
+let splice = newArray.splice(3, 2); 
+console.log(splice, newArray);
+
+// every: true if all items is true, vice versa false == AND
+console.log(splice.every(item => typeof item == 'string'));
+console.log(newArray.every(item => typeof item == 'number'));
+
+// some: true if one of the items is true, vice verse false == OR
+console.log(splice.some(item => typeof item == 'number'));
+console.log(newArray.some(item => typeof item == 'number'));
+
+// filter: return all matched array
+console.log(newArray.filter(item => item === "Adam"));
+
+// find: return fisrt ele of matched array
+console.log(newArray.find(item => item === "Adam"));
+
+// findIndex: same as find but return index instead
+console.log(newArray.findIndex(item => item === "Adam"));
+
+// foreach
+let newStr = '';
+newArray.forEach((item, index) => {
+    if (index < newArray.length - 1) newStr += item + ",";
+    else newStr += item;
+});
+console.log(newArray, newStr);
+
+// includes
+console.log(newArray.includes("Adam"));
+
+// reduce: (previous, current), initialValue
+// [0, 1, [2, [3, [4, [5]]]]]
+// [0, 1, 2, [3, [4, [5]]]]
+// [0, 1, 2, 3, [4, [5]]]
+// [0, 1, 2, 3, 4, [5]]
+// [0, 1, 2, 3, 4, 5]
+let list = [0, [1, [2, [3, [4, [5]]]]]];
+const flatten = arr => arr.reduce((previous, current) => previous.concat(
+    Array.isArray(current) ? flatten(current) : current // recursive
+), [])
+console.log(flatten(list));
+
+// Example
+let products = [
+    {name: "Hat", price: 24.5, stock: 10},
+    {name: "Kayak", price: 289.99, stock: 1},
+    {name: "Soccer Ball", price: 10, stock: 0},
+    {name: "Running Shoes", price: 116.50, stock: 20}
+]
+let totalValue = products
+    .filter(item => item.stock > 0)
+    .reduce((prev, item) => prev + (item.price * item.stock), 0);
+    console.log("Total value: $" + totalValue.toFixed(2));
